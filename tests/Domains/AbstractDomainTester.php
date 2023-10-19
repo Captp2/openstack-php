a8f5705fac93b274d25f6f09a8ce073b03bd3b45
A Gaudot <axel@lespetitsbots.com>
2023-10-19 22:11:30 +0200

    Forgotten file

--- tests/Domains/AbstractDomainTester.php
+++ tests/Domains/AbstractDomainTester.php
@@ -1,22 +1,27 @@
 <?php
 
-namespace OvhSwift\Tests;
+namespace OvhSwift\Tests\Domains;
 
 use OvhSwift\Domain\AbstractDomain;
 use OvhSwift\Exceptions\InvalidConfigException;
+use OvhSwift\Providers\AbstractAccessor;
 use PHPUnit\Framework\TestCase;
 
 abstract class AbstractDomainTester extends TestCase
 {
     protected AbstractDomain $domain;
+    protected AbstractAccessor $accessor;
 
     public function __construct(string $name)
     {
         $domainName = $this->getDomainName();
-        $this->domain = new $domainName();
+        $this->domain = new $domainName($this->getGetter(), $this->getSetter());
 
         parent::__construct($name);
     }
 
     abstract function getDomainName(): string;
+
+    abstract function getGetter(): AbstractAccessor;
+    abstract function getSetter(): AbstractAccessor;
 }
